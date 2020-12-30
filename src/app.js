7a58307d531bf993d5097454ec1721b5ffb6002c
Davidson Ratis <davidsonmakarios@gmail.com>
2020-12-30 17:02:50 -0300

    importing middlewares, routes and configuring 404 and 500 errors

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const port = 3030
+const middlewares = require('./config/middlewares.js')
+const routes = require('./routes/routes.js')
+const app = require('express')()
+
+middlewares(app)
+routes(app)
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  return res.json({ error: error.message })
+})
+
+app.listen(port, () => {
+  console.log(`
+App running at port:
+-> http://localhost:${port}`)
+})
+
+

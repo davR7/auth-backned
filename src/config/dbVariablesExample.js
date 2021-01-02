39e21c4fc63e6d954d61522c1f2a6a478c35f423
Davidson Ratis <davidsonmakarios@gmail.com>
2021-01-01 21:45:18 -0300

    example files

---
+++ src/config/dbVariablesExample.js
@@ -0,0 +1,11 @@
+/* Change the file name to db, so that it is used by mogoose in: 
+   database/index.js */
+const config = {
+    user: "",
+    pass: "",
+    host: "",
+    port: "",
+    db: ""
+}
+
+module.exports = config
---
+++ src/config/secretKeyExample.js
@@ -0,0 +1,4 @@
+/* Change the file or create a new one with the name secretKey */
+module.exports = {
+    key: ''
+}
